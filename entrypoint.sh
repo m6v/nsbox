25c6b80eb70c13a6feae3ef51331d466c8a1f323
Sergey Maksimov <m6v@mail.ru>
2026-08-26 12:54:02 +0300

    Create images function

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,11 +8,11 @@ on_exit() {
 }
 trap "on_exit" EXIT SIGTERM SIGINT
 
-# Включение виртуального интерфейса, с настройками в /etc/network/interfaces
+# Включение виртуального интерфейса с настройками в /etc/network/interfaces
 # ifup host0
 # Включение виртуального интерфейса с явно указанными настройками
-ip link set dev host0 up
-ip addr add 192.168.100.2/24 dev host0
+# ip link set dev host0 up
+# ip addr add 192.168.100.2/24 dev host0
 
 # Запуск sleep в фоне и ожидание с помощью wait, который перехватывает сигналы завершения работы
 sleep infinity &
